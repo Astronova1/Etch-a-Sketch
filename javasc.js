af10bc6c8b623cef9e0983ebfe181d4e2c4ceb5b
Awaiz <awaiz123nasir123@gmail.com>
2025-04-23 19:55:26 +0500

    add 16x16 divs

--- javasc.js
+++ javasc.js
@@ -1,18 +1,34 @@
-const toAdd = document.createDocumentFragment();
 
-for(let i=0; i<16; i++){
-    let grid = document.createElement("div");
-    grid.className = 'grid '+ (i+1);
-    toAdd.appendChild(grid);
- }
+
+function createDivs(app){
+   const toAdd = document.createDocumentFragment();
+   for(let i=0; i<16; i++){
+      app = document.createElement("div");
+      app.className = 'grid '+ (i+1);
+      toAdd.appendChild(app);
+   }
+   return toAdd;
+}
+
+function createVerticalDivs(con){
+   const toAddMain = document.createDocumentFragment();
+   for(let i=0; i<16 ; i++){
+      con = document.createElement("cont");
+      con.className = 'cont' + (i+1);
+      toAddMain.appendChild(con);
+      main.appendChild(con);
+      con.appendChild(createDivs(con));
+   }
+}
 
  const body = document.querySelector("body");
-   body.setAttribute("style","display:flex");
+   body.setAttribute("style","display:flex;");
 
- const cont = document.createElement("container");
- body.appendChild(cont);
- cont.appendChild(toAdd);
+const main = document.createElement("main");
+   main.setAttribute("style","display:flex; border:2px solid blue; flex-direction: column");
+   body.appendChild(main);
 
+ createVerticalDivs("cont");
 
  const grids = document.querySelectorAll(".grid");
  grids.forEach(grid => {
