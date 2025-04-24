322e41e90e5fc89c76f46e4be1fc272a6626f481
Awaiz <awaiz123nasir123@gmail.com>
2025-04-24 19:38:28 +0500

    fix flex not working in the grid

--- javasc.js
+++ javasc.js
@@ -13,8 +13,8 @@ function createDivs(app){
 function createVerticalDivs(con){
    const toAddMain = document.createDocumentFragment();
    for(let i=0; i<16 ; i++){
-      con = document.createElement("cont");
-      con.className = 'cont' + (i+1);
+      con = document.createElement("div");
+      con.className = 'cont ' + (i+1);
       toAddMain.appendChild(con);
       main.appendChild(con);
       con.appendChild(createDivs(con));
@@ -25,12 +25,18 @@ function createVerticalDivs(con){
    body.setAttribute("style","display:flex;");
 
 const main = document.createElement("main");
-   main.setAttribute("style","display:flex; border:2px solid blue; flex-direction: column");
+   main.setAttribute("style","display:flex; border:2px solid blue; flex: 1; margin:0px");
    body.appendChild(main);
 
  createVerticalDivs("cont");
 
+ const conts = document.querySelectorAll(".cont");
+ conts.forEach(cont => {
+   cont.setAttribute("style","display: flex; flex-direction: column; margin: 0px; border: 0px ; padding: 0px ;flex: 1 1 auto; border: 2px solid yellow");
+ })
+
+
  const grids = document.querySelectorAll(".grid");
  grids.forEach(grid => {
-   grid.setAttribute("style","width: 50px; height: 20px; border:2px solid red")
- })
+   grid.setAttribute("style","width:80px; height:40px;border:2px solid red; flex: 1 1 auto");
+ });
