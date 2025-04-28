91f2f3271aeb3c4a85922cb826649a52eefd9b1c
Awaiz <awaiz123nasir123@gmail.com>
2025-04-28 21:25:32 +0500

    add hovereffect and fix gap in divs

--- javasc.js
+++ javasc.js
@@ -2,7 +2,7 @@
 
 function createDivs(app){
    const toAdd = document.createDocumentFragment();
-   for(let i=0; i<16; i++){
+   for(let i=0; i<8; i++){
       app = document.createElement("div");
       app.className = 'grid '+ (i+1);
       toAdd.appendChild(app);
@@ -12,7 +12,7 @@ function createDivs(app){
 
 function createVerticalDivs(con){
    const toAddMain = document.createDocumentFragment();
-   for(let i=0; i<16 ; i++){
+   for(let i=0; i<8 ; i++){
       con = document.createElement("div");
       con.className = 'cont ' + (i+1);
       toAddMain.appendChild(con);
@@ -26,18 +26,25 @@ function createVerticalDivs(con){
    body.setAttribute("style","display:flex; padding: 0px;");
 
 const main = document.createElement("main");
-   main.setAttribute("style","display:flex; border:0px solid blue; flex: 1 1 0; margin: 0px;");
+   main.setAttribute("style","display:flex; border:0px solid blue; flex: 1 1 0; margin: 0px;justify-content: center;");
    body.appendChild(main);
 
  createVerticalDivs("cont");
 
  const conts = document.querySelectorAll(".cont");
  conts.forEach(cont => {
-   cont.setAttribute("style","display: flex; flex-direction: column; margin: 0px; border: 0px; padding: 0px;;flex: 1 1 0; border: 1px solid yellow");
+   cont.setAttribute("style","display: flex; flex-direction: column; margin: 0px; border: 0px; padding: 0px;;flex: 0 1 0; border: 1px solid yellow;");
  })
 
 
  const grids = document.querySelectorAll(".grid");
  grids.forEach(grid => {
    grid.setAttribute("style","width:80px; height:50px;border:0px solid red; flex: 1 1 auto; display: flex");
+ });
+
+ grids.forEach(grid => {
+   grid.addEventListener("mouseover", () => {
+   grid.style.backgroundColor = "blue";    
+ })
+
  });
